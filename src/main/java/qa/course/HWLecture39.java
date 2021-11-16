830fb16f39dcc3a06d3027a232b94db6a52f43b8
SevdalinaTrifonova <sevdalina.trifonova@gmail.com>
2021-11-16 23:20:07 +0200

    Homework Lecture 39

--- src/main/java/qa/course/HWLecture39.java
+++ src/main/java/qa/course/HWLecture39.java
@@ -2,18 +2,17 @@ package qa.course;
 
 import java.util.Scanner;
 
-
-public class HWLecture39 {
+public class HWLecture39
 // Console input-output of name and hobby
 
-    public static void main(String[] args) {
-
+{
+    public static void main(String[] args)
+    {
         Scanner scn = new Scanner(System.in);
         System.out.println("Please input your name:");
         String name = scn.nextLine();
         System.out.println("Please write your hobby:");
         String hobby = scn.nextLine();
-        System.out.println("You are " + name + " and your hobby is "+hobby);
-
+        System.out.println("You are " + name + " and your hobby is "+ hobby);
     }
 }
